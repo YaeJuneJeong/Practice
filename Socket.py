b6483e3280eab74d1394d9efa1825ada00080b0d
YaeJuneJeong <jyj98071@gmail.com>
2021-02-22 17:40:55 +0900

    Send_img

--- Socket.py
+++ Socket.py
@@ -1,9 +1,8 @@
-
 import cv2
 import socketio
 import base64
 import time
-
+# setting Yju-guest
 sio = socketio.Client()
 sio.connect('http://172.26.3.62:3001')
 
@@ -22,7 +21,12 @@ def connect():
     # sio.disconnect()
 @sio.event
 def connect():
-    sio.emit('response',{'data':'usname'})
+    while True:
+        ret, fname = cap.read()
+        result, frame = cv2.imencode('.jpg', fname, encode_param)
+        data = base64.b64encode(frame)
+        sio.emit('req_image',data)
+        
 @sio.on("msg")
 def msg():
     print("msg")
