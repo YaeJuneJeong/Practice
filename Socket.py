cc29e7e0b324e273b8c0f5b1a6882d5abeec2175
YaeJuneJeong <jyj98071@gmail.com>
2021-02-22 17:23:27 +0900

    socket_proto

---
+++ Socket.py
@@ -0,0 +1,31 @@
+
+import cv2
+import socketio
+import base64
+import time
+
+sio = socketio.Client()
+sio.connect('http://172.26.3.62:3001')
+
+cap = cv2.VideoCapture(0)
+encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+ret , fname = cap.read()
+result , frame = cv2.imencode('.jpg',fname,encode_param)
+data = base64.b64encode(frame)
+
+
+@sio.on
+def connect():
+    print('connect')
+
+    cap.release()
+    # sio.disconnect()
+@sio.event
+def connect():
+    sio.emit('response',{'data':'usname'})
+@sio.on("msg")
+def msg():
+    print("msg")
+
+sio.connect('http://172.26.3.62:3001')
+# sio.wait()
