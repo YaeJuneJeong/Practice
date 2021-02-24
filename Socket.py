5324b0b8766521ab07fc7105c404d2bd0f7f8416
YaeJuneJeong <jyj98071@gmail.com>
2021-02-24 14:17:33 +0900

    picture

--- Socket.py
+++ Socket.py
@@ -2,6 +2,12 @@ import cv2
 import socketio
 import base64
 import time
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(('8.8.8.8', 1))  # connect() for UDP doesn't send packets
+local_ip_address = s.getsockname()[0]
+print(local_ip_address)
 # setting Yju-guest
 sio = socketio.Client()
 sio.connect('http://172.26.3.62:3001')
@@ -26,10 +32,10 @@ def connect():
         result, frame = cv2.imencode('.jpg', fname, encode_param)
         data = base64.b64encode(frame)
         sio.emit('req_image',data)
-        
+
 @sio.on("msg")
 def msg():
     print("msg")
 
 sio.connect('http://172.26.3.62:3001')
-# sio.wait()
+sio.wait()
